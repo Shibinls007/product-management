4cebf43ff5485270cb2fadf4c646c1ee718c8978
Shibinls007 <shibinls007@gmail.com>
2025-06-16 23:38:41 +0530

    Initial commit

--- src/main/java/com/sls/product_management/dto/ProductDTO.java
+++
@@ -1,8 +0,0 @@
-package com.sls.product_management.dto;
-
-import com.sls.product_management.model.Product;
-
-import java.util.List;
-import java.util.stream.Collectors;
-public class ProductDTO {
-}
